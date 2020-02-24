fca8a01b4f0c07ab47ef3d56903e70b9d34a9e6e
Dieter Mutiz <dieter_md@hotmail.com>
2020-02-24 10:05:02 -0300

    Prep for production

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,6 +36,11 @@ const CheckoutPage = ({cartItems, total}) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="text-warning">
+      *Please use the following test credit card for payments*
+      <br/>
+      4242 4242 4242 4242 - CVV: 123
+    </div>
     <StripeCheckoutButton price={total} />
   </div>
 );
